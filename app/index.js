9130616fc9fc3de8b1ccea4337be4022418deddd
Bryan <bryan.davila@charismamedia.com>
2017-08-11 14:08:44 -0400

    moved classes to separate files

--- app/index.js
+++ app/index.js
@@ -1,98 +1,29 @@
-import React from 'react';
+import React    from 'react';
 import ReactDOM from 'react-dom';
+import Articles from './Articles';
+import Beats    from './Beats';
+import Game     from './Game';
 import './index.css';
+
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>{this.props.test}</h1>
+                <Beats />
+                <Game />
+                <Articles />
             </div>
         )
     }
 }
 
-class Articles extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {stories: []};
-    }
-
-    componentDidMount() {
-        //console.log('CALLED');
-        let reqTest = "./data/data.json";
-        fetch(reqTest)
-            .then((response) => { return response.json(); })
-            .then((data) => {
-                //data.forEach(function(info){
-                    //console.log(data);
-                    this.setState({
-                         stories: data
-                    });
-                    // console.log(this.state.stories[0].title);
-                //});
-            })
-            .catch((err) => {
-                console.error(err);
-            });
-    }
-
-    render() {
-        //console.log(this.state.stories[1]);
-        return (
-            <div>
-                {this.state.stories.map((storie,index) => {
-                    return <div key={index+'div'}>
-                                <h1 key={index}>{storie.title}</h1>
-                                <p key={index+'para'}>{storie.body}</p>
-                            </div>                    
-                })}
-            </div>
-        );
-    }
-}
-
-class Clock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: "Load"};
-    }
-
-    componentDidMount() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    tick() {
-        let now = new Date();
-        let makeBeat = (now.getUTCSeconds() + (now.getUTCMinutes() * 60) + ((now.getUTCHours()+1) * 3600)) / 86.4;
-        let beat = Math.round(makeBeat);
-        this.setState({
-            date: beat
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>Hey</h1>
-                <h2>It's @{this.state.date.toString()} BEATS.</h2>
-            </div>
-        );
-    }
-}
-
 ReactDOM.render(
-    <Clock />,
+    <App />,
     document.getElementById('app')
 );
 
+//https://jsonplaceholder.typicode.com/photos
 // getData() {
 //     let reqTest = "https://jsonplaceholder.typicode.com/posts";
 //     fetch(reqTest)
