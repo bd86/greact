a727c9ab3285846425881171c069452b31be4ca8
Bryan <bryan.davila@charismamedia.com>
2017-08-11 14:10:16 -0400

    Render datetime in swatch internet time

---
+++ app/Beats.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class Beats extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: "Load"};
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        let now = new Date();
+        let makeBeat = (now.getUTCSeconds() + (now.getUTCMinutes() * 60) + ((now.getUTCHours()+1) * 3600)) / 86.4;
+        let beat = Math.round(makeBeat);
+        this.setState({
+            date: beat
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hey</h1>
+                <h2>It's @{this.state.date.toString()} BEATS.</h2>
+            </div>
+        );
+    }
+}
+
+export default Beats;
