0862f3a5fba1a4379096f136d4e145219d3fc6f2
Bryan <bryan.davila@charismamedia.com>
2017-08-17 17:05:08 -0400

    changed form component and removed some place holder text

--- app/index.js
+++ app/index.js
@@ -1,17 +1,17 @@
-import React    from 'react';
-import ReactDOM from 'react-dom';
-import Articles from './Articles';
-import Beats    from './Beats';
-import Game     from './Game';
-import Form     from './Form';
+import React      from 'react';
+import ReactDOM   from 'react-dom';
+import Articles   from './Articles';
+import Beats      from './Beats';
+import Game       from './Game';
+import EmailForm  from './EmailForm';
+import ShipToForm from './ShipToForm';
 import './index.css';
-
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Form />
+                <ShipToForm />
             </div>
         )
     }
@@ -21,15 +21,3 @@ ReactDOM.render(
     <App />,
     document.getElementById('app')
 );
-
-//https://jsonplaceholder.typicode.com/photos
-// getData() {
-//     let reqTest = "https://jsonplaceholder.typicode.com/posts";
-//     fetch(reqTest)
-//         .then(function(response) { return response.json(); })
-//         .then(function(data) {
-//             for (let i = 0; i < data.length; i++) {
-//                 console.log(data[i].title);
-//             }
-//         });
-// }
