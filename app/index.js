06026e49fb053736c974e06a44cc2783019c8fc1
Bryan <bryan.davila@charismamedia.com>
2017-08-30 17:36:10 -0400

    changed component

--- app/index.js
+++ app/index.js
@@ -3,15 +3,13 @@ import ReactDOM   from 'react-dom';
 import Articles   from './Articles';
 import Beats      from './Beats';
 import Game       from './Game';
-import EmailForm  from './EmailForm';
-import ShipToForm from './ShipToForm';
 import './index.css';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <ShipToForm />
+                <Beats />
             </div>
         )
     }
