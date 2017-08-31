9b2ec5c6d984e5ad01936b499300a029f56b728c
Bryan <bryan.davila@charismamedia.com>
2017-08-31 17:09:38 -0400

    changed component and changed tab size

--- app/index.js
+++ app/index.js
@@ -1,21 +1,19 @@
 import React      from 'react';
 import ReactDOM   from 'react-dom';
-import Articles   from './Articles';
 import Beats      from './Beats';
-import Game       from './Game';
 import './index.css';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <Beats />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Beats />
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 );
